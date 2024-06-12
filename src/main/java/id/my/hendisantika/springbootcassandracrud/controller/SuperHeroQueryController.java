e6bc27d90e1de82060fdbef224c6a6df1b7972bc
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 15:58:13 +0700

    Add getSuperHeroByName in SuperHeroQueryController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroQueryController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroQueryController.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.springbootcassandracrud.controller;
 import id.my.hendisantika.springbootcassandracrud.model.SuperHero;
 import id.my.hendisantika.springbootcassandracrud.service.SuperHeroQueryService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -14,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,5 +49,16 @@ public class SuperHeroQueryController {
     @GetMapping
     public List<SuperHero> getAll() {
         return superHeroQueryService.getAll();
+    }
+
+    @Operation(summary = "Get all Superheroes by name using query")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Superheroes list",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = SuperHero.class)))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    @GetMapping("/name/{name}")
+    public List<SuperHero> getSuperHeroByName(@Parameter(description = "Superhero name to be fetched") @PathVariable String name) {
+        return superHeroQueryService.getSuperHeroByName(name);
     }
 }
