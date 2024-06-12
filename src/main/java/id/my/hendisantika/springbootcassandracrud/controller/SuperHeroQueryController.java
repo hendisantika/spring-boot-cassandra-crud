e4ab42c64949379353905a701b748782cebd73ed
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 15:58:01 +0700

    Add getAll in SuperHeroQueryController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroQueryController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroQueryController.java
@@ -1,12 +1,23 @@
 package id.my.hendisantika.springbootcassandracrud.controller;
 
+import id.my.hendisantika.springbootcassandracrud.model.SuperHero;
 import id.my.hendisantika.springbootcassandracrud.service.SuperHeroQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +37,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class SuperHeroQueryController {
 
     private final SuperHeroQueryService superHeroQueryService;
+
+    @Operation(summary = "Get all Superheroes using query")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Superheroes list",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = SuperHero.class)))}),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    @GetMapping
+    public List<SuperHero> getAll() {
+        return superHeroQueryService.getAll();
+    }
 }
