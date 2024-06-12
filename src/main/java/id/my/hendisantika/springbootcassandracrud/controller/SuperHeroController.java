43f73ca56a2b132610d5bf5c8518192c52e4acf2
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 15:59:50 +0700

    Add findById in SuperHeroController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroController.java
@@ -2,6 +2,7 @@ package id.my.hendisantika.springbootcassandracrud.controller;
 
 import id.my.hendisantika.springbootcassandracrud.model.SuperHero;
 import id.my.hendisantika.springbootcassandracrud.service.SuperHeroService;
+import io.micrometer.common.util.StringUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,6 +17,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,5 +69,25 @@ public class SuperHeroController {
         log.info("Superheroes fetched from DB :: {}", list);
 
         return ResponseEntity.ok().body(list);
+    }
+
+    @Operation(summary = "Get a Superhero by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the Superhero",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = SuperHero.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Superhero not found", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<SuperHero> findById(@PathVariable Long id) {
+        log.info("*** Getting Superhero from DB for Id :: {}", id);
+        SuperHero superHero = superHeroService.findById(id);
+
+        if (StringUtils.isEmpty(superHero.getName()))
+            return ResponseEntity.notFound().build();
+
+        log.info("Superhero fetched :: {}", superHero);
+        return ResponseEntity.ok().body(superHero);
     }
 }
