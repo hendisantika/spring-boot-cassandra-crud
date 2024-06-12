0bcd4ce27b04bcd8640d9eaae0af8fa0e4ffabc3
hendisantika <hendisantika@yahoo.co.id>
2024-06-12 15:50:39 +0700

    Add save in SuperHeroController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/SuperHeroController.java
@@ -1,10 +1,20 @@
 package id.my.hendisantika.springbootcassandracrud.controller;
 
+import id.my.hendisantika.springbootcassandracrud.model.SuperHero;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +33,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class SuperHeroController {
 
     private final SuperHeroService superHeroService;
+
+    @Operation(summary = "Save dummy Superheroes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
+    @GetMapping("/save")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void save() {
+        log.info("*** Storing dummy static data to DB ***");
+        List<SuperHero> list = superHeroService.save();
+        log.info("Stored data to DB :: {}", list);
+    }
 }
