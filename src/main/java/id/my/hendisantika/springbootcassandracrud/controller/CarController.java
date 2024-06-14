2b07f16fe0bc53c32979af64145ad3a920557f02
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 08:59:11 +0700

    Add Spring Doc API CarController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/CarController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/CarController.java
@@ -2,6 +2,11 @@ package id.my.hendisantika.springbootcassandracrud.controller;
 
 import id.my.hendisantika.springbootcassandracrud.model.Car;
 import id.my.hendisantika.springbootcassandracrud.service.CarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,26 +35,47 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/cars")
 @RequiredArgsConstructor
+@Tag(name = "Cars", description = "Cars CRUD API with documentation annotations")
 public class CarController {
 
     private final CarService carservice;
 
     @GetMapping()
+    @Operation(summary = "Get All Cars")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<List<Car>> getAllCars() {
         return new ResponseEntity<>(carservice.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get Car by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<Car> getCarById(@PathVariable("id") String id) {
         return new ResponseEntity<>(carservice.findById(UUID.fromString(id)), HttpStatus.OK);
     }
 
     @PostMapping()
+    @Operation(summary = "Add New Car")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<Car> createCar(@RequestBody Car car) {
         return new ResponseEntity<>(carservice.saveCar(car), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update Car by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<Car> updateCar(@PathVariable("id") String id, @RequestBody Car car) {
         if (carservice.existById(UUID.fromString(id))) {
             return new ResponseEntity<>(carservice.saveCar(car), HttpStatus.ACCEPTED);
@@ -59,6 +85,11 @@ public class CarController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Car by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Saved superheroes list"),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     public ResponseEntity<HttpStatus> deleteCar(@PathVariable("id") String id) {
         carservice.deleteCar(UUID.fromString(id));
 
