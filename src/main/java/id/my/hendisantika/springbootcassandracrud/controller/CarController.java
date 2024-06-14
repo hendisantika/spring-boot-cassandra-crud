2bc9269601419702869dfb816f30200a132c8bb4
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 08:54:26 +0700

    Add getAllCars in CarController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/CarController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/CarController.java
@@ -1,9 +1,15 @@
 package id.my.hendisantika.springbootcassandracrud.controller;
 
+import id.my.hendisantika.springbootcassandracrud.model.Car;
 import id.my.hendisantika.springbootcassandracrud.service.CarService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,8 @@ public class CarController {
 
     private final CarService carservice;
 
+    @GetMapping()
+    public ResponseEntity<List<Car>> getAllCars() {
+        return new ResponseEntity<>(carservice.findAll(), HttpStatus.OK);
+    }
 }
