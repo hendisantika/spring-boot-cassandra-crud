5713f9f1ad0c024c7959e5b59a1e08ffda8a1ff8
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 08:54:38 +0700

    Add getCarById in CarController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootcassandracrud/controller/CarController.java
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/controller/CarController.java
@@ -6,10 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +33,10 @@ public class CarController {
     @GetMapping()
     public ResponseEntity<List<Car>> getAllCars() {
         return new ResponseEntity<>(carservice.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Car> getCarById(@PathVariable("id") String id) {
+        return new ResponseEntity<>(carservice.findById(UUID.fromString(id)), HttpStatus.OK);
     }
 }
