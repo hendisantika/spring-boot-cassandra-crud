aa6c05a8fc5a189006086ab9e5de5d3e3fab535d
hendisantika <hendisantika@yahoo.co.id>
2024-06-14 08:52:31 +0700

    Add CarService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootcassandracrud/service/CarService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.springbootcassandracrud.service;
+
+import id.my.hendisantika.springbootcassandracrud.repository.CarRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-cassandra-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 6/14/24
+ * Time: 08:52
+ * To change this template use File | Settings | File Templates.
+ */
+@AllArgsConstructor
+@Service
+public class CarService {
+
+    private final CarRepository carRepository;
+}
